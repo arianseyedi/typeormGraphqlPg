4e8b925e0ba0b53d18f2ca33317a8dcfdf7a1667
arian.seyedi@gmail.com <arian.seyedi@gmail.com>
2018-08-13 16:31:08 -0400

    done with cards queries and implementations

--- src/entities/card.ts
+++ src/entities/card.ts
@@ -4,33 +4,29 @@ import {
     Column,
     CreateDateColumn,
     UpdateDateColumn,
+    BaseEntity,
 } from 'typeorm';
 
 
 @Entity('cards')
-export class Card {
+export class Card extends BaseEntity {
 
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
-
     @CreateDateColumn()
     created_at: Date;
-
 
     @UpdateDateColumn()
     updated_at: Date;
 
-
     @Column('text')
     title: string;
-
 
     @Column('text', {
         nullable: true,
     })
     description: string;
-
 
     @Column('boolean', {
         default: "false",
