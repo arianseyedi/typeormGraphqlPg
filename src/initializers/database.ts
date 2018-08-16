e19c621a26392beedec63aa3520e4308c84423e7
arian.seyedi@gmail.com <arian.seyedi@gmail.com>
2018-08-16 09:55:39 -0400

    security fix

--- src/initializers/database.ts
+++ src/initializers/database.ts
@@ -8,19 +8,21 @@ import { Card } from 'entities/card';
 
 export const databaseInitializer = async () => {
 
-    return await createConnection({
-        type     : 'postgres',
-        host     : 'localhost',
-        port     : 5432,
-        username : 'postgres',
-        password : 'tester',
-        database : 'test1',
-        entities: [
-            "src/entities/**/*.ts",
-        ],
-        logging: ['query', 'error'],
-        synchronize: true,
-    }).then((connection) => {
+    return await createConnection(
+        // {
+        //     type     : 'postgres',
+        //     host     : 'localhost',
+        //     port     : 5432,
+        //     username : 'postgres',
+        //     password : 'tester',
+        //     database : 'test1',
+        //     entities: [
+        //         "src/entities/**/*.ts",
+        //     ],
+        //     logging: ['query', 'error'],
+        //     synchronize: true,
+        // }
+    ).then((connection) => {
         console.log('Database connection established');
     });
     console.log('Database connection ended');
